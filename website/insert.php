090824ffec664648fd76d37dae4ab271e4257058
Tyre King <johns28y@student.gpc.edu>
2016-06-19 11:01:45 -0400

    no message

--- website/insert.php
+++ website/insert.php
@@ -1,12 +1,12 @@
-$conn = new PDO ($dsn, $username, $password );
+$conn = new PDO (bulletinboard, root, " ");
 if (!$connection) {
     die('Connection failed', mysql_error());
 }
 if(isset $_POST['submit']) {
     $title = $_POST['title'];
     $body = $_POST['body'];
-    $timestamp = date("Y-m-d h:i:sa");
-    $query = mysql_query("insert into post(title,body,date) values ($title, $body, $timestamp)");
+    $timestamp = date('Y-m-d H:i:s');
+    $query = mysql_query("INSERT INTO posts (title, body, date) VALUES ($title, $body, $timestamp)");
     }
 mysql_close($connection);
 
