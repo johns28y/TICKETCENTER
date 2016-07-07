4dce6f7b5998bef84568b60a63b030403190bd75
Tyre Johnson-King <johns28y@student.gpc.edu>
2016-07-07 19:09:00 -0400

    Add files via upload

--- Website/home.php
+++ Website/home.php
@@ -12,7 +12,7 @@
             Ticket Center
             </h5>
     <form action = ""> 
-       <input type = "button" value = "Purchase"  onclick = "window.location.href ='ticketindex.php';"/>
+       <input type = "button" value = "Purchase"  onclick = "window.location.href ='sportticketindex.php';"/>
        <input type = "button" value = "Sell" onclick = "window.location.href = 'sportcomposer.php';"/>  
       <input type = "button" value = "Cancel" onclick = "window.location.href = 'ticketcancellation.php';"/>                                               
     </form>
