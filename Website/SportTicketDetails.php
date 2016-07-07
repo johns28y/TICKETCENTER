6715dcd5783193d07192e03792e41f50d9463f2d
Tyre Johnson-King <johns28y@student.gpc.edu>
2016-07-07 19:09:29 -0400

    Add files via upload

---
+++ Website/SportTicketDetails.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html>  
+    <head>
+        <link rel = "stylesheet" type = "text/css" href = "styles.css">
+        <title>
+        Ticket Center
+        </title>
+    </head>
+    <body>
+        <div class = "ticket-list">
+            <h1>
+            Ticket Center
+            </h1>
+            <a href = "home.php"> 
+                Go Home<br>
+            </a> 
+        </div>
+<?php
+$dsn = 'mysql:host=127.0.0.1; dbname=bulletinboard';
+$username = 'root';
+$password = 'GoogleScholar1'; 
+$connection = new PDO($dsn, $username, $password); 
+if(!$connection){
+    die('Connection failed' + mysql_error());
+}
+          
+        $getId=$_GET["id"];
+        $sql = 'SELECT * FROM posts where id='.$getId; 
+        //input phone number variable above        
+foreach($connection->query($sql) as $row){
+    $title = $row['title'];
+    $body = $row['body'];
+    $id = $row['id'];
+    //$date = $row['date']."\n";
+    echo '<div class = "ticket">';
+    echo $title;
+    echo $body;
+    echo '</div>';
+}
+      
+?>
+        
+   </body>
+</html>
+
+    
