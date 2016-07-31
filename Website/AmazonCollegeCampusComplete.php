14887456b0dcc775bc16d075faf9cff6b6e4aefa
Tyre Johnson-King <johns28y@student.gpc.edu>
2016-07-31 15:16:06 -0400

    Add files via upload
    An update done.

---
+++ Website/AmazonCollegeCampusComplete.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+    <link href = 'styles.css' rel = 'stylesheet' type = "text/css"/>
+        <title>
+           Ticket Center
+        </title>
+    </head>
+    <body>
+        <div class = 'ticket-list'>
+        <img src = "Amazonlogo.png" height = "100" width = "100">
+        </div>
+        <br>
+        <br>
+        <br>
+    </body>
+</html>
+<br>
+<br>
+<br>
+Thank you!
+<br>
+<br>
+<br>
+ <input type = "button" value = "Completely finished"  onclick = "window.location.href ='Home.php';"/>    
