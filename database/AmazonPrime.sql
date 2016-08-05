96431273dc3c5d962c39c624931ac15180845cda
Tyre Johnson-King <johns28y@student.gpc.edu>
2016-08-05 15:50:45 -0400

    Add files via upload

---
+++ database/AmazonPrime.sql
@@ -0,0 +1,6 @@
+/*A Component of my relational database*/
+CREATE TABLE AmazonPrime(
+amazonprime_id int NOT NULL AUTO_INCREMENT, 
+phonenumber VARCHAR(25) NOT NULL,
+PRIMARY KEY (amazonprime_id)
+);
---
+++ database/Concert.sql
@@ -0,0 +1,22 @@
+/*A Component of my relational database*/
+CREATE TABLE Concert( 
+    concert_id INT NOT NULL AUTO_INCREMENT,
+    amazonprime_id INT,
+    squarecash_id INT,
+    sport VARCHAR(255) NOT NULL,
+    hometeam VARCHAR(255) NOT NULL,
+    awayteam VARCHAR(255) NOT NULL,
+    starathlete1 VARCHAR(255) NOT NULL,
+    starathlete2 VARCHAR(255) NOT NULL,
+    stadium VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    gametime VARCHAR(255) NOT NULL, 
+    seat VARCHAR(255) NOT NULL, 
+    INDEX (amazonprime_id),
+    INDEX (squarecash_id),
+    PRIMARY KEY (concert_id),
+    FOREIGN KEY (amazonprime_id) REFERENCES AmazonPrime(amazonprime_id) ON DELETE CASCADE,
+    FOREIGN KEY (squarecash_id) REFERENCES  SquareCash(squarecash_id) ON DELETE CASCADE
+);
+
+
