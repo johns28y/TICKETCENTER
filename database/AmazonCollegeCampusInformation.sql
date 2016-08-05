b9e65fac3985e48c61cb037b82c23e8c7de527d0
Tyre Johnson-King <johns28y@student.gpc.edu>
2016-08-05 15:49:16 -0400

    Delete AmazonCollegeCampusInformation.sql

--- database/AmazonCollegeCampusInformation.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE AmazonCollegeCampusInformation(
-Amazon_id int NOT NULL, 
-PhoneNumber int NOT NULL, 
-Sport_id int, 
-Concert_id int,
-PRIMARY KEY (Amazon_id), 
-FOREIGN KEY (Sport_id) REFERENCES Sportposts(Sport_id)
-FOREIGN KEY (Concert_id)REFERENCES Concertposts(Concert_id)
-) ENGINE = InnoDB;
