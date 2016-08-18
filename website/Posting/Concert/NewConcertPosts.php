4cb80dd8ff7c9718a98fd2b49d9f9c63f8ea0cb1
Tyre Johnson-King <johns28y@student.gpc.edu>
2016-08-18 17:01:46 -0400

    Update NewConcertPosts.php

--- website/Posting/Concert/NewConcertPosts.php
+++ website/Posting/Concert/NewConcertPosts.php
@@ -1,5 +1,5 @@
 <?php 
-$dsn = 'mysql:host=127.0.0.1; dbname=bulletinboard';
+$dsn = 'mysql:host=127.0.0.1; dbname=TICKETCENTER';
 $username = 'root';
 $password = 'GoogleScholar1'; 
 $connection = new PDO($dsn, $username, $password); 
