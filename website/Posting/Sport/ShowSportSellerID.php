803ea2c333b8d91905081635e35079196fc5a115
Tyre Johnson-King <johns28y@student.gpc.edu>
2016-08-18 17:03:06 -0400

    Update ShowSportSellerID.php

--- website/Posting/Sport/ShowSportSellerID.php
+++ website/Posting/Sport/ShowSportSellerID.php
@@ -16,7 +16,7 @@
         </form>
         </div>
 <?php
-$dsn = 'mysql:host=127.0.0.1; dbname=bulletinboard';
+$dsn = 'mysql:host=127.0.0.1; dbname=TICKETCENTER';
 $username = 'root';
 $password = 'GoogleScholar1';
 $connection = new PDO ($dsn, $username, $password);
